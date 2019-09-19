3296317bc7bd184762a489a97372f48b41a09f51
tecnico <tecnico@Repository>
2019-09-19 15:54:53 +0200

    nuevas

---
+++ Socket/src/cta/Consulta.java
@@ -0,0 +1,10 @@
+package cta;
+
+import java.util.Vector;
+
+//Una consulta es un vector de modulos + filtro
+public class Consulta {
+ Vector<Modulo> modulos = new Vector<Modulo>();
+ String filtro;
+ 
+}
---
+++ Socket/src/cta/Modulo.java
@@ -0,0 +1,19 @@
+package cta;
+
+public class Modulo {
+	String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMask() {
+		return mask;
+	}
+	public void setMask(String mask) {
+		this.mask = mask;
+	}
+	String mask;
+	
+}
